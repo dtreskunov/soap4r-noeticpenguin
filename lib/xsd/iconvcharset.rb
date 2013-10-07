904b0edea94c4dbab352090b64f5454a0d01cf86
RobertZK <technoguyrob@gmail.com>
2013-10-07 13:25:43 -0500

    Update iconvcharset.rb

--- lib/xsd/iconvcharset.rb
+++ lib/xsd/iconvcharset.rb
@@ -19,21 +19,20 @@ module XSD
         str.encode(to, from, :invalid => :replace, :undef => :replace, :replace => '?')
       end
     else
-      def self.safe_iconv(to, from, str)
-        iconv = Iconv.new(to, from)
-        out = ""
-        begin
-          out << iconv.iconv(str)
-        rescue Iconv::IllegalSequence => e
-          out << e.success
-          ch, str = e.failed.split(//, 2)
-          out << '?'
-          warn("Failed to convert #{ch}")
-          retry
-        end
-        return out
+    def self.safe_iconv(to, from, str)
+      iconv = Iconv.new(to, from)
+      out = ""
+      begin
+        out << iconv.iconv(str)
+      rescue Iconv::IllegalSequence => e
+        out << e.success
+        ch, str = e.failed.split(//, 2)
+        out << '?'
+        warn("Failed to convert #{ch}")
+        retry
       end
+      return out
     end
-
+  end
 
 end
